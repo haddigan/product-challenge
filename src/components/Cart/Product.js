41c8ebdc513d9204f50f5a8c4bc99b3c72e59970
Dan <haddigan@gmail.com>
2020-01-11 18:18:05 -0500

    add some product details

--- src/components/Cart/Product.js
+++ src/components/Cart/Product.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-const Product = ({ quantity, price }) => (
-  <li>
-    {quantity}x ${price}
-  </li>
-);
+import stripTags from "utils/stripTags";
+
+const Product = ({ quantity, price, details }) => {
+  const { images, name, description, size } = details;
+  const [image] = images;
+  return (
+    <li>
+      <img src={image.thumb} alt={name} />
+      <dl>
+        <dt>Product Name:</dt>
+        <dd>{name}</dd>
+        <dt>Quantity:</dt>
+        <dd>{quantity}</dd>
+        <dt>Total Price:</dt>
+        <dd>{price}</dd>
+        <dt>Description:</dt>
+        <dd>{stripTags(description)} </dd>
+        {size && (
+          <>
+            <dt>Size:</dt>
+            <dd>{size}</dd>
+          </>
+        )}
+      </dl>
+    </li>
+  );
+};
 
 export default Product;

5249ffe644e8e5fee7470a4bfbe3a891a9e84f3d
Dan <haddigan@gmail.com>
2020-01-11 18:49:59 -0500

    pass coupon price to component

--- src/components/Cart/ProductList.js
+++ src/components/Cart/ProductList.js
@@ -4,15 +4,23 @@ import Product from "./Product";
 
 const ProductList = ({ products, productDetails }) => {
   const renderProducts = products => {
-    return products.map(({ quantity, product_id: id, price }) => {
-      const details = productDetails.find(
-        ({ product_id }) => product_id === id
-      );
+    return products.map(
+      ({ quantity, product_id: id, price, credit_coupon_price }) => {
+        const details = productDetails.find(
+          ({ product_id }) => product_id === id
+        );
 
-      return (
-        <Product key={id} price={price} quantity={quantity} details={details} />
-      );
-    });
+        return (
+          <Product
+            key={id}
+            couponPrice={credit_coupon_price}
+            price={price}
+            quantity={quantity}
+            details={details}
+          />
+        );
+      }
+    );
   };
   return <ul>{renderProducts(products)}</ul>;
 };

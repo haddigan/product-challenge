e3f040c44ad0930236a8f9bd3017257be90b9369
Dan <haddigan@gmail.com>
2020-01-11 17:07:09 -0500

    export product base url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const baseUrl = process.env.REACT_APP_BASE_URL;
+export const baseUrl = process.env.REACT_APP_BASE_URL || "";
+export const prodBaseUrl = process.env.REACT_APP_PROD_BASE_URL || "";

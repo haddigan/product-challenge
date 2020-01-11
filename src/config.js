7107983e8c461ba6f8060d258973eee325ec21c3
Dan <haddigan@gmail.com>
2020-01-11 17:14:07 -0500

    add locationId to .env

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const baseUrl = process.env.REACT_APP_BASE_URL || "";
 export const prodBaseUrl = process.env.REACT_APP_PROD_BASE_URL || "";
+export const locationId = process.env.REACT_APP_LOCATION_ID || "-1";

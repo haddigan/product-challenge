d575a854177e2d57f15294445f796c386519957b
Dan <haddigan@gmail.com>
2020-01-11 16:45:14 -0500

    create useSubtotal hook for calculating subtotal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Dinero from "dinero.js";
 import { baseUrl } from "config.js";
+
+import ProductList from "./ProductList";
 
 const useFetchCart = () => {
   const [data, setData] = useState({});
@@ -14,14 +17,39 @@ const useFetchCart = () => {
   return data;
 };
 
-const Cart = () => {
-  const { cart, payment_method, postal_code, user } = useFetchCart();
+const useSubtotal = (cartProducts = []) => {
+  const [subtotal, setSubtotal] = useState(0);
+  useEffect(() => {
+    const initial = Dinero({ amount: 0, currency: "USD" });
 
+    const subtotal = cartProducts.reduce((accumulatedTotal, currentItem) => {
+      const { price, quantity } = currentItem;
+
+      const integerPrice = Math.round(price * 100);
+      const itemTotal = Dinero({
+        amount: integerPrice,
+        currency: "USD"
+      }).multiply(quantity);
+      return accumulatedTotal.add(itemTotal);
+    }, initial);
+    setSubtotal(subtotal);
+  }, [cartProducts]);
+  return subtotal ? [subtotal.toFormat("$0,0.00")] : [null];
+};
+
+const Cart = () => {
+  const { cart = {}, payment_method, postal_code, user } = useFetchCart();
+  const { products } = cart;
+  const [subtotal] = useSubtotal(products);
   return (
-    <div>
+    <section>
       <h1>Cart</h1>
-      <div>{JSON.stringify({ cart, payment_method, postal_code, user })}</div>
-    </div>
+      <div>
+        {products && <ProductList products={products} />}
+        {JSON.stringify({ payment_method, postal_code, user })}
+      </div>
+      {subtotal && <div>subtotal: {subtotal}</div>}
+    </section>
   );
 };
 
